d8305331258befe8a37a765e178faacccab73e46
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-09-22 20:51:37 -0300

    Updated styles

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,38 +1,163 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 
 import Container from '../../components/Container';
 import { User, RepoList, Button } from './styles';
 
-export default function Details() {
-  return (
-    <Container>
-      <User>
-        <Link to="/">
-          <Button>
-            <FaArrowLeft /> Voltar
-          </Button>
-        </Link>
-        <img src="https://avatars0.githubusercontent.com/u/1?v=4" alt="user" />
-        <h1>login</h1>
-        <p>userid</p>
-      </User>
+export default class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      details: {
+        id: 35873365,
+        login: 'frederikocmr',
+        profile_url: 'https://api.github.com/users/frederikocmr',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/35873365?v=4',
+        login_creation: '2018-01-27T16:41:07Z',
+      },
+      repositories: [
+        {
+          id: 122229486,
+          name: 'AngularAdmin',
+          url: 'https://api.github.com/repos/frederikocmr/AngularAdmin',
+        },
+        {
+          id: 119603832,
+          name: 'AngularFirebaseApp',
+          url: 'https://api.github.com/repos/frederikocmr/AngularFirebaseApp',
+        },
+        {
+          id: 205736603,
+          name: 'car-bot-ia',
+          url: 'https://api.github.com/repos/frederikocmr/car-bot-ia',
+        },
+        {
+          id: 137438833,
+          name: 'ComputacaoGraficaC',
+          url: 'https://api.github.com/repos/frederikocmr/ComputacaoGraficaC',
+        },
+        {
+          id: 161072009,
+          name: 'ControleEstoquePHP',
+          url: 'https://api.github.com/repos/frederikocmr/ControleEstoquePHP',
+        },
+        {
+          id: 209412748,
+          name: 'FIPE',
+          url: 'https://api.github.com/repos/frederikocmr/FIPE',
+        },
+        {
+          id: 150365842,
+          name: 'IndicadorDeCarros_Watson_ChatBot',
+          url:
+            'https://api.github.com/repos/frederikocmr/IndicadorDeCarros_Watson_ChatBot',
+        },
+        {
+          id: 145254591,
+          name: 'InquilioApp',
+          url: 'https://api.github.com/repos/frederikocmr/InquilioApp',
+        },
+        {
+          id: 135861950,
+          name: 'Ionic_Test_App',
+          url: 'https://api.github.com/repos/frederikocmr/Ionic_Test_App',
+        },
+        {
+          id: 145501626,
+          name: 'Java8CRUD',
+          url: 'https://api.github.com/repos/frederikocmr/Java8CRUD',
+        },
+        {
+          id: 203695610,
+          name: 'MERN_Stack',
+          url: 'https://api.github.com/repos/frederikocmr/MERN_Stack',
+        },
+        {
+          id: 204107451,
+          name: 'Node_Challenge',
+          url: 'https://api.github.com/repos/frederikocmr/Node_Challenge',
+        },
+        {
+          id: 205578747,
+          name: 'Node_Complete_Backend',
+          url:
+            'https://api.github.com/repos/frederikocmr/Node_Complete_Backend',
+        },
+        {
+          id: 135636922,
+          name: 'ProjetoCppQt',
+          url: 'https://api.github.com/repos/frederikocmr/ProjetoCppQt',
+        },
+        {
+          id: 147443138,
+          name: 'Rainbow-Android-SDK-Samples',
+          url:
+            'https://api.github.com/repos/frederikocmr/Rainbow-Android-SDK-Samples',
+        },
+        {
+          id: 206222990,
+          name: 'ReactJS_App',
+          url: 'https://api.github.com/repos/frederikocmr/ReactJS_App',
+        },
+        {
+          id: 205977980,
+          name: 'ReactJS_Base_App',
+          url: 'https://api.github.com/repos/frederikocmr/ReactJS_Base_App',
+        },
+        {
+          id: 200392744,
+          name: 'ReactNativeCattleManagement',
+          url:
+            'https://api.github.com/repos/frederikocmr/ReactNativeCattleManagement',
+        },
+        {
+          id: 207063553,
+          name: 'ReactNative_App',
+          url: 'https://api.github.com/repos/frederikocmr/ReactNative_App',
+        },
+      ],
+      loading: false,
+    };
+  }
 
-      <RepoList>
-        <li>
-          <div>
-            <strong>repo</strong>
-            <p>repo</p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <strong>repo</strong>
-            <p>repo</p>
-          </div>
-        </li>
-      </RepoList>
-    </Container>
-  );
+  render() {
+    const { details, repositories, loading } = this.state;
+
+    return (
+      <Container>
+        <User>
+          <Link to="/">
+            <Button>
+              <FaArrowLeft /> Return
+            </Button>
+          </Link>
+
+          <img src={details.avatar_url} alt={details.login} />
+          <h1>{details.login}</h1>
+          <p>
+            <strong>ID:</strong> {details.id}
+          </p>
+          <p>
+            <strong>Created at: </strong>
+            {new Date(details.login_creation).toLocaleString()}
+          </p>
+          <p>
+            <strong>URL:</strong> {details.profile_url}
+          </p>
+        </User>
+
+        <RepoList>
+          {repositories.map(repo => (
+            <li>
+              <div>
+                <strong>{repo.name}</strong>
+                <p>#{repo.id}</p>
+              </div>
+            </li>
+          ))}
+        </RepoList>
+      </Container>
+    );
+  }
 }
--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,59 +1,194 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FaGithub } from 'react-icons/fa';
 
 import Container from '../../components/Container';
 import { Header, UsersList, StyledLink } from './styles';
 
-export default function Users() {
-  return (
-    <Container>
-      <Header>
-        <FaGithub /> <h1>Usuários</h1>
-      </Header>
-      <UsersList>
-        <StyledLink to="/details/freds">
-          <li>
-            <img
-              src="https://avatars0.githubusercontent.com/u/1?v=4"
-              alt="user"
-            />
-            <div>
-              <strong> Teste </strong>
-              <p>Teste</p>
-            </div>
-          </li>
-        </StyledLink>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/1?v=4"
-            alt="user"
-          />
-          <div>
-            <strong> Teste </strong>
-            <p>Teste</p>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/1?v=4"
-            alt="user"
-          />
-          <div>
-            <strong> Teste </strong>
-            <p>Teste</p>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/1?v=4"
-            alt="user"
-          />
-          <div>
-            <strong> Teste </strong>
-            <p>Teste</p>
-          </div>
-        </li>
-      </UsersList>
-    </Container>
-  );
+export default class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          id: 1,
+          login: 'mojombo',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        },
+        {
+          id: 2,
+          login: 'defunkt',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
+        },
+        {
+          id: 3,
+          login: 'pjhyett',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
+        },
+        {
+          id: 4,
+          login: 'wycats',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/4?v=4',
+        },
+        {
+          id: 5,
+          login: 'ezmobius',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/5?v=4',
+        },
+        {
+          id: 6,
+          login: 'ivey',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/6?v=4',
+        },
+        {
+          id: 7,
+          login: 'evanphx',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/7?v=4',
+        },
+        {
+          id: 17,
+          login: 'vanpelt',
+          avatar_url: 'https://avatars1.githubusercontent.com/u/17?v=4',
+        },
+        {
+          id: 18,
+          login: 'wayneeseguin',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/18?v=4',
+        },
+        {
+          id: 19,
+          login: 'brynary',
+          avatar_url: 'https://avatars0.githubusercontent.com/u/19?v=4',
+        },
+        {
+          id: 20,
+          login: 'kevinclark',
+          avatar_url: 'https://avatars3.githubusercontent.com/u/20?v=4',
+        },
+        {
+          id: 21,
+          login: 'technoweenie',
+          avatar_url: 'https://avatars3.githubusercontent.com/u/21?v=4',
+        },
+        {
+          id: 22,
+          login: 'macournoyer',
+          avatar_url: 'https://avatars3.githubusercontent.com/u/22?v=4',
+        },
+        {
+          id: 23,
+          login: 'takeo',
+          avatar_url: 'https://avatars3.githubusercontent.com/u/23?v=4',
+        },
+        {
+          id: 25,
+          login: 'caged',
+          avatar_url: 'https://avatars3.githubusercontent.com/u/25?v=4',
+        },
+        {
+          id: 26,
+          login: 'topfunky',
+          avatar_url: 'https://avatars3.githubusercontent.com/u/26?v=4',
+        },
+        {
+          id: 27,
+          login: 'anotherjesse',
+          avatar_url: 'https://avatars3.githubusercontent.com/u/27?v=4',
+        },
+        {
+          id: 28,
+          login: 'roland',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/28?v=4',
+        },
+        {
+          id: 29,
+          login: 'lukas',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/29?v=4',
+        },
+        {
+          id: 30,
+          login: 'fanvsfan',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/30?v=4',
+        },
+        {
+          id: 31,
+          login: 'tomtt',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/31?v=4',
+        },
+        {
+          id: 32,
+          login: 'railsjitsu',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/32?v=4',
+        },
+        {
+          id: 34,
+          login: 'nitay',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/34?v=4',
+        },
+        {
+          id: 35,
+          login: 'kevwil',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/35?v=4',
+        },
+        {
+          id: 36,
+          login: 'KirinDave',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/36?v=4',
+        },
+        {
+          id: 37,
+          login: 'jamesgolick',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/37?v=4',
+        },
+        {
+          id: 38,
+          login: 'atmos',
+          avatar_url: 'https://avatars3.githubusercontent.com/u/38?v=4',
+        },
+        {
+          id: 44,
+          login: 'errfree',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/44?v=4',
+        },
+        {
+          id: 45,
+          login: 'mojodna',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/45?v=4',
+        },
+        {
+          id: 46,
+          login: 'bmizerany',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/46?v=4',
+        },
+      ],
+      loading: false,
+    };
+  }
+
+  render() {
+    const { users, loading } = this.state;
+
+    return (
+      <Container>
+        <Header>
+          <FaGithub /> <h1>Users</h1>
+        </Header>
+        <UsersList>
+          {users.map(user => (
+            <StyledLink
+              key={user.id}
+              to={`/details/${encodeURIComponent(user.login)}`}
+            >
+              <li>
+                <img src={user.avatar_url} alt="user" />
+                <div>
+                  <strong> {user.login} </strong>
+                  <p>#{user.id}</p>
+                </div>
+              </li>
+            </StyledLink>
+          ))}
+        </UsersList>
+      </Container>
+    );
+  }
 }
